2d0724f264bc1af2c14cbdd82b1aeb9469544c4d
pradipkumarmadheshiya <prdpmadheshiya@gmail.com>
2024-06-17 21:18:01 +0530

    completed till evaluation 2

--- evaluation_1_1/Website/Main/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
-
-
-def home(request):
-    context={"message":"Welcome to the website....."}
-    return render(request, "Main/home.html", context)
